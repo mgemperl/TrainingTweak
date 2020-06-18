a75320c65e88faa1170c6c3e96eefedf7f3ca438
mgemperl <markusgemply@gmail.com>
2020-06-17 19:44:41 -0600

    Deleted addressed TODOs.

--- TroopTraining/SubModule.cs
+++ TroopTraining/SubModule.cs
@@ -18,10 +18,6 @@ namespace TrainingTweak
 
         private Harmony _harmony;
 
-        // TODO: Update module_strings.xml 
-        // TODO: Update training behavior to use mod-internal perk and training field xp amounts
-        // TODO: Implement harmony patch to set perk and training field xp amounts to 0
-
         protected override void OnSubModuleLoad()
         {
             base.OnSubModuleLoad();
